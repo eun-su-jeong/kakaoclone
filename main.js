d3255e102ab4da437c452408f7a3d761682ff84b
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2025-02-04 16:01:08 +0900

    style: scroll 이벤트 추가

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+	/* 드롭다운 토글 */
 	const dropDownBtn = document.querySelector('.drop-down-btn');
 
 	dropDownBtn.addEventListener('click', () => {
 		dropDownBtn.classList.toggle('active');
 	});
+
+	/* Intersection Observer */
+	const options = {
+		root: null,
+		rootMargin: '0px',
+		threshold: 0.1,
+	};
+
+	const observer = new IntersectionObserver((entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('active');
+
+				observer.unobserve(entry.target);
+			} else {
+				entry.target.classList.remove('active');
+			}
+		});
+	}, options);
+
+	const elements = document.querySelectorAll('.active-on-visible');
+	elements.forEach((element) => observer.observe(element));
 });
